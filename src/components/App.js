015262b60b9ce67805179091d335bf7ccbe4de74
Komil <komildeveloper111@gmail.com>
2021-01-14 22:29:20 +0500

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class App extends Component {
+  state = { language: 'uzbek', color: 'primary', text: 'Select a language :' };
+
+  onLanguageAndOnColorChange = (language, color, text) => {
+    this.setState({ language, color, text });
+  };
+
+  render() {
+    return (
+      <div className='ui container'>
+        <div>
+          {this.state.language === 'english'
+            ? 'Select a language :'
+            : 'Tilni tanlang :'}
+          <i
+            className='flag us'
+            onClick={() =>
+              this.onLanguageAndOnColorChange('english', 'primary')
+            }
+            style={{ cursor: 'pointer', marginLeft: '5px' }}
+          />
+          <i
+            className='flag uz'
+            onClick={() => this.onLanguageAndOnColorChange('uzbek', 'red')}
+            style={{ cursor: 'pointer' }}
+          />
+        </div>
+        <ColorContext.Provider value={this.state.color}>
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Field.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+class Field extends Component {
+  static contextType = LanguageContext;
+
+  render() {
+    const text = this.context === 'english' ? 'Name' : 'Ism';
+    return (
+      <div className='field'>
+        <label>{text}</label>
+        <input type='text' />
+      </div>
+    );
+  }
+}
+
+export default Field;
